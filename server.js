07f92de5f2f1bfff03e24ac417b1ed88ec53d3da
Michał Brusiło <mibru@interia.pl>
2018-06-19 12:48:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const fs = require('fs')
+
+let app = express();
+let stringifyFile;
+
+app.listen(3000);
+
+app.use(bodyParser.json());
+
+app.get('/getNote', function (req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+	    if (err) throw err;
+	    stringifyFile = data
+	    res.send(data);
+	});
+});
+
+app.post('/updateNote/:note', function (req, res) {
+	stringifyFile += '{"'+ req.params.note + '"}';
+	fs.writeFile('./test.json', stringifyFile, function(err) {
+	    if (err) throw err;
+	    console.log('file updated');
+	});
+	 res.send(stringifyFile);
+});
+
